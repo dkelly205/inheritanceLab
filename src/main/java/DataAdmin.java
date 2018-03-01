2fda8ac78ff426ed6fdca825a35cf1ee893e5048
daniel kelly <danieljkelly91@gmail.com>
2018-03-01 21:48:11 +0000

    added developer class and developerTest

---
+++ src/main/java/DataAdmin.java
@@ -0,0 +1,6 @@
+public class DataAdmin extends Employee {
+
+    public DataAdmin(String name, String niNumber, double salary){
+        super(name, niNumber, salary);
+    }
+}
---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Danny", "JL0000111", 24000.00);
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Danny", developer.getName());
+    }
+
+    @Test
+    public void getNiNumber(){
+        assertEquals("JL0000111", developer.getNiNumber());
+    }
+
+    @Test
+    public void getSalary(){
+        assertEquals(24000.00, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void payBonus(){
+        assertEquals(240.00, developer.payBonus(), 0.01);
+    }
+
+    @Test
+    public void raiseSalary(){
+        developer.raiseSalary(0.01);
+        assertEquals(24240.00, developer.getSalary(), 0.01);
+    }
+}
